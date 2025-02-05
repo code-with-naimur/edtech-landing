599e9e03158ab6906f2330f69cdafeba3f2ccdd8
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2025-02-05 23:38:39 +0600

    persist dark mode, add: profile page top nav bar

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function toggleDarkMode() {
   document.body.classList.toggle('dark-mode')
+  const themeSwitch = document.querySelector('.theme-switch')
+  themeSwitch.classList.toggle('justify-end')
+
+  // Save the current mode to local storage
+  const isDarkMode = document.body.classList.contains('dark-mode')
+  localStorage.setItem('darkMode', isDarkMode ? 'enabled' : 'disabled')
 }
+
+// Check the saved mode on page load
+document.addEventListener('DOMContentLoaded', () => {
+  const savedMode = localStorage.getItem('darkMode')
+  if (savedMode === 'enabled') {
+    document.body.classList.add('dark-mode')
+    const themeSwitch = document.querySelector('.theme-switch')
+    themeSwitch.classList.add('justify-end')
+  }
+})
 
 // You can call this function on a button click or any other event
 document
